d274680fa46ad9a3a16215ebcd57ea224eab4fe9
andyming1314 <andyming@aliyun.com>
2025-01-13 22:23:26 +0800

    fix:[task]：TaskSubTypesInitFactory去掉loadSubTypesObject加载逻辑，让SubCompoment实现bean初始化

--- eleven-vertx/src/main/java/hx/nine/eleven/vertx/autoconfig/TaskSubTypesInitFactory.java
+++ eleven-vertx/src/main/java/hx/nine/eleven/vertx/autoconfig/TaskSubTypesInitFactory.java
@@ -19,28 +19,28 @@ public class TaskSubTypesInitFactory implements ApplicationSubTypesInitFactory {
 
 	@Override
 	public void loadSubTypesObject(Reflections reflections) throws Throwable {
-		Set<Class<? extends ElevenScheduledTask>> elevenScheduledTask = reflections.getSubTypesOf(ElevenScheduledTask.class);
-		Set<Class<? extends ElevenThreadTask>> elevenThreadTask = reflections.getSubTypesOf(ElevenThreadTask.class);
-		if (elevenScheduledTask != null && elevenScheduledTask.size() > 0){
-			Set<ElevenScheduledTask> subTypesOfBeans = new HashSet<>(elevenScheduledTask.size());
-			elevenScheduledTask.forEach(t->{
-				ConstructorAccess constructorAccess = ConstructorAccess.get(t);
-				ElevenScheduledTask obj = (ElevenScheduledTask)constructorAccess.newInstance();
-				subTypesOfBeans.add(obj);
-
-			});
-			DefaultElevenApplicationContext.build().addSubTypesOfBean(ElevenScheduledTask.class,subTypesOfBeans);
-		}
-
-		if (elevenThreadTask != null && elevenThreadTask.size() > 0){
-			Set<ElevenThreadTask> subTypesOfBeans = new HashSet<>(elevenThreadTask.size());
-			elevenThreadTask.forEach(t->{
-				ConstructorAccess constructorAccess = ConstructorAccess.get(t);
-				ElevenThreadTask obj = (ElevenThreadTask)constructorAccess.newInstance();
-				subTypesOfBeans.add(obj);
-
-			});
-			DefaultElevenApplicationContext.build().addSubTypesOfBean(ElevenThreadTask.class,subTypesOfBeans);
-		}
+//		Set<Class<? extends ElevenScheduledTask>> elevenScheduledTask = reflections.getSubTypesOf(ElevenScheduledTask.class);
+//		Set<Class<? extends ElevenThreadTask>> elevenThreadTask = reflections.getSubTypesOf(ElevenThreadTask.class);
+//		if (elevenScheduledTask != null && elevenScheduledTask.size() > 0){
+//			Set<ElevenScheduledTask> subTypesOfBeans = new HashSet<>(elevenScheduledTask.size());
+//			elevenScheduledTask.forEach(t->{
+//				ConstructorAccess constructorAccess = ConstructorAccess.get(t);
+//				ElevenScheduledTask obj = (ElevenScheduledTask)constructorAccess.newInstance();
+//				subTypesOfBeans.add(obj);
+//
+//			});
+//			DefaultElevenApplicationContext.build().addSubTypesOfBean(ElevenScheduledTask.class,subTypesOfBeans);
+//		}
+//
+//		if (elevenThreadTask != null && elevenThreadTask.size() > 0){
+//			Set<ElevenThreadTask> subTypesOfBeans = new HashSet<>(elevenThreadTask.size());
+//			elevenThreadTask.forEach(t->{
+//				ConstructorAccess constructorAccess = ConstructorAccess.get(t);
+//				ElevenThreadTask obj = (ElevenThreadTask)constructorAccess.newInstance();
+//				subTypesOfBeans.add(obj);
+//
+//			});
+//			DefaultElevenApplicationContext.build().addSubTypesOfBean(ElevenThreadTask.class,subTypesOfBeans);
+//		}
 	}
 }
