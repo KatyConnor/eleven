7c5e17bbf3c12882818e66d81c50967a0531be32
bob <2825523288@qq.com>
2025-01-21 17:38:39 +0800

    fix：[exception]: exception错误对象添加statusCode，和responseBody保持一致

--- eleven-domain/src/main/java/hx/nine/eleven/domain/obj/vo/ErrorVO.java
+++ eleven-domain/src/main/java/hx/nine/eleven/domain/obj/vo/ErrorVO.java
@@ -1,13 +1,23 @@
 package hx.nine.eleven.domain.obj.vo;
 
 import hx.nine.eleven.msg.code.code.MessageCode;
+import hx.nine.eleven.msg.code.enums.SystemCode;
 
 /**
  * 错误信息数据
  */
 public class ErrorVO extends BaseVO {
-
+    /**
+     * 交易状态码
+     */
+    private String statusCode = SystemCode.FAILED.getCode();
+    /**
+     * 业务编码
+     */
     private String code;
+    /**
+     * 业务提示信息
+     */
     private String message;
 
     public ErrorVO(){}
@@ -20,6 +30,14 @@ public class ErrorVO extends BaseVO {
     public void setErrorMessageCode(MessageCode messageCode){
         this.code = messageCode.getCode();
         this.message = messageCode.getMessage();
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
     }
 
     public String getMessage() {
